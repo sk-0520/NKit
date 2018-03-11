fcd88cb14bf8d5468ce7626d517864e1a848a486
sk <content.type.text.net@gmail.com>
2018-03-11 19:40:47 +0900

    MnMn からビルド周りパクってきた

---
+++ Build/build.bat
@@ -0,0 +1,46 @@
+cd /d %~dp0\..\
+echo off
+
+set BUILD=Build
+set ERROR=%BUILD%\@error
+set PROJECT=NKit
+
+set OUTPUT=Output\Release
+set OUTPUT_x86=%OUTPUT%\AnyCPU\%PROJECT%
+set VER_TARGET=%OUTPUT_x86%\NKit.exe
+set ZIP=%BUILD%\zip.vbs
+set GV=%BUILD%\get-ver.vbs
+
+set DOTNETVER=v4.0.30319
+
+del /Q %ERROR%
+
+rmdir /S /Q "%OUTPUT_x86%"
+
+rem if "%PROCESSOR_ARCHITECTURE%" NEQ "x86" (
+rem 	set MB=%windir%\microsoft.net\framework64\%DOTNETVER%\msbuild
+rem ) else (
+rem 	set MB=%windir%\microsoft.net\framework\%DOTNETVER%\msbuild
+rem )
+set MB=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
+
+echo build x86
+"%MB%" Source\NKit.sln /p:DefineConstants="BUILD;%1" /p:Configuration=Release;Platform="Any CPU" /t:Rebuild /m /p:TargetFrameworkVersion=v4.7 /p:CodeAnalysisRuleSet=
+set ERROR_X86=%ERRORLEVEL%
+
+if not %ERROR_X86% == 0 echo "build error x86: %ERROR_X86%" >> "%ERROR%"
+
+for /F "usebackq" %%s in (`cscript "%GV%" "%VER_TARGET%"`) do set EXEVER=%%s
+
+if "%2" == "FULL" goto REMOVED
+
+echo remove
+echo remove *.pdb, *.xml
+del /S /Q *.pdb
+del /S /Q "%OUTPUT_x86%\lib\*.xml"
+
+:REMOVED
+
+echo compression
+cscript "%ZIP%" "%OUTPUT_x86%" "%OUTPUT%\NKit_%EXEVER%_AnyCPU.zip"
+
---
+++ build.sh
@@ -0,0 +1,45 @@
+#!💩
+
+# CIで止まられるとなんもできん
+if [ -z "${CI+x}" ] ; then
+    if [ `git status -s | wc -l` -ne 0 ] ; then
+        git status
+        echo "There is changed files. Press Any key to exit ... "
+        read
+        exit 1
+    fi
+else
+    echo "[CI] skip: status check"
+fi
+
+# バージョン書き換え
+VERSION_REV=`git rev-parse HEAD`
+find -name 'AssemblyInfo.cs' -print0 | xargs -0 sed -E -i "s/^\[\s*assembly\s*:\s*\AssemblyInformationalVersion\s*\(\s*\"\s*(xxxxxxxxxxxxxxDEVELOPMENTxxxxxxxxxxxxxxx)\s*\"\s*\)\s*\]/[assembly: AssemblyInformationalVersion(\"$VERSION_REV\")]/"
+# <YEAR>書き換え
+find -name 'AssemblyInfo.cs' -print0 | xargs -0 sed -E -i "s/<YEAR>/`date +%Y`/"
+
+# ビルド
+pushd Build
+    cmd.exe //c build.bat ${BUILD_TYPE} ${BUILD_OUTPUT}
+popd
+
+# バージョン戻し
+git reset --hard
+
+echo ""
+if [ -f Build/@error ] ; then
+    if [ -z "${CI+x}" ] ; then
+        echo "!!!build failed!!! Press Any key to exit ..."
+        read
+    else
+        echo "!!!build failed!!! CI mode."
+    fi
+else
+    if [ -z "${CI+x}" ] ; then
+        echo "build success. Press Any key to exit ..."
+        read
+    else
+        echo "build success. CI mode."
+    fi
+fi
+
