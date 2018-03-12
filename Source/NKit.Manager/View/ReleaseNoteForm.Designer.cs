13e375f202b862718405434e2cfe56004327d1ac
sk <content.type.text.net@gmail.com>
2018-03-12 20:26:24 +0900

    リリースノート表示用フォームの側だけ作成

---
+++ Source/NKit.Manager/View/ReleaseNoteForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ContentTypeTextNet.NKit.Manager.View
+{
+    partial class ReleaseNoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.commandClose = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // commandClose
+            // 
+            this.commandClose.Location = new System.Drawing.Point(160, 267);
+            this.commandClose.Name = "commandClose";
+            this.commandClose.Size = new System.Drawing.Size(75, 23);
+            this.commandClose.TabIndex = 0;
+            this.commandClose.Text = "close";
+            this.commandClose.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(46, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(391, 203);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // ReleaseNoteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 312);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.commandClose);
+            this.Name = "ReleaseNoteForm";
+            this.Text = "ReleaseNoteForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button commandClose;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ Source/NKit.Manager/View/ReleaseNoteForm.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContentTypeTextNet.NKit.Manager.View
+{
+    public partial class ReleaseNoteForm : Form
+    {
+        public ReleaseNoteForm()
+        {
+            InitializeComponent();
+
+            Font = SystemFonts.MessageBoxFont;
+        }
+
+        #region property
+        #endregion
+
+        #region function
+        #endregion
+    }
+}
