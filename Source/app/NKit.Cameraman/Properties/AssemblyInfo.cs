8764d3eb79487b5fb8adfb69a2ca87f098462b0f
sk <content.type.text.net@gmail.com>
2018-03-14 00:11:38 +0900

    アセンブリ周り手直し
    あとで一括で変えたいね

--- Source/app/NKit.Cameraman/Properties/AssemblyInfo.cs
+++ Source/app/NKit.Cameraman/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("NKit.Cameraman")]
+[assembly: AssemblyTitle("ContentTypeTextNet.NKit.Cameraman")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NKit.Cameraman")]
+[assembly: AssemblyProduct("ContentTypeTextNet.NKit.Cameraman")]
 [assembly: AssemblyCopyright("Copyright © 2018-<YEAR> sk <content-type-text.net>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
