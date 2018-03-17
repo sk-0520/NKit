2d396788c28948bf6bcae45672ba11ccf6ba9101
sk <content.type.text.net@gmail.com>
2018-03-17 12:29:40 +0900

    ログの処理みたいに他プログラム起動を単独で出来るようにしたい、デバッグ時にしんどい

---
+++ Source/app/NKit.Main/Model/ApplicationSwitcher.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.NKit.Common;
+using ContentTypeTextNet.NKit.Utility.Model;
+
+namespace ContentTypeTextNet.NKit.Main.Model
+{
+    public class ApplicationSwitcher: ModelBase
+    {
+        public ApplicationSwitcher(Uri serviceUri)
+        {
+            if(serviceUri != null) {
+                ApplicationClient = new NKitApplicationTalkerClient(NKitApplicationKind.Main, serviceUri);
+            }
+        }
+
+        #region property
+
+        NKitApplicationTalkerClient ApplicationClient { get; }
+
+        #endregion
+
+        #region function
+
+        #endregion
+    }
+}
