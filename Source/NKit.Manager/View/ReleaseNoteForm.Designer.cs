4ee1761b3204b1fe7116eb4c11d49975130809d2
sk <content.type.text.net@gmail.com>
2018-03-20 10:36:08 +0900

    リリースノート表示ウィンドウ画面からでもアップデートを開始できるようにした

--- Source/NKit.Manager/View/ReleaseNoteForm.Designer.cs
+++ Source/NKit.Manager/View/ReleaseNoteForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ContentTypeTextNet.NKit.Manager.View
+namespace ContentTypeTextNet.NKit.Manager.View
 {
     partial class ReleaseNoteForm
     {
@@ -29,6 +29,7 @@
         {
             this.commandClose = new System.Windows.Forms.Button();
             this.viewReleaseNote = new System.Windows.Forms.WebBrowser();
+            this.commandUpdate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // commandClose
@@ -50,6 +51,17 @@
             this.viewReleaseNote.Size = new System.Drawing.Size(421, 259);
             this.viewReleaseNote.TabIndex = 1;
             // 
+            // commandUpdate
+            // 
+            this.commandUpdate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.commandUpdate.Location = new System.Drawing.Point(28, 277);
+            this.commandUpdate.Name = "commandUpdate";
+            this.commandUpdate.Size = new System.Drawing.Size(75, 23);
+            this.commandUpdate.TabIndex = 0;
+            this.commandUpdate.Text = "update";
+            this.commandUpdate.UseVisualStyleBackColor = true;
+            this.commandUpdate.Click += new System.EventHandler(this.commandUpdate_Click);
+            // 
             // ReleaseNoteForm
             // 
             this.AcceptButton = this.commandClose;
@@ -57,6 +69,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.commandClose;
             this.ClientSize = new System.Drawing.Size(482, 312);
+            this.Controls.Add(this.commandUpdate);
             this.Controls.Add(this.commandClose);
             this.Controls.Add(this.viewReleaseNote);
             this.Name = "ReleaseNoteForm";
@@ -69,5 +82,6 @@
 
         private System.Windows.Forms.Button commandClose;
         private System.Windows.Forms.WebBrowser viewReleaseNote;
+        private System.Windows.Forms.Button commandUpdate;
     }
 }
