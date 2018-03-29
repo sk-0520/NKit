e5f12425a18cd9e3d167093c8e3272faaf0da602
sk <content.type.text.net@gmail.com>
2018-03-29 15:44:35 +0900

    枠用のフォーム作成

---
+++ Source/app/NKit.Cameraman/View/FrameForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContentTypeTextNet.NKit.Cameraman.View
+{
+    public partial class FrameForm : Form
+    {
+        public FrameForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
