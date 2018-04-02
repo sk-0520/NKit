94ac97059eaad987b546150cd970de98146c29d6
sk <content.type.text.net@gmail.com>
2018-04-02 13:18:56 +0900

    PDF検索用クラスの作成(スタブ)

---
+++ Source/app/NKit.Main/Model/Searcher/PdfSearcher.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.NKit.Main.Model.Searcher
+{
+    public abstract class PdfSearchResultBase : SearchResultBase
+    { }
+
+    public class PdfSearcher
+    {
+        #region property
+        #endregion
+
+        #region function
+        #endregion
+    }
+}
