4557e615cca09877108f7de32741598e7350c3af
sk <content.type.text.net@gmail.com>
2018-04-05 18:55:40 +0900

    グッチャグチャなりそうなので分離

--- Source/app/NKit.Main/Model/Finder/FindGroupModel.cs
+++ Source/app/NKit.Main/Model/Finder/FindGroupModel.cs
@@ -266,36 +266,48 @@ namespace ContentTypeTextNet.NKit.Main.Model.Finder
             }
         }
 
-        void OutputListFileCore(TextWriter writer, bool absolutePath, bool isSimple, IReadOnlyList<int> outputitemsIndex)
+        static void WriteListFileTitle(TextWriter writer, IReadOnlyFindGroupSetting setting, int count)
         {
             // 数は少ないけど、一旦はこれで
+
             var titleMap = new Dictionary<string, string>() {
-                ["COUNT"] = outputitemsIndex.Count.ToString(),
-                ["DIR"] = CurrentCache.Setting.RootDirectoryPath,
-                ["NAME"] = CurrentCache.Setting.FileNameSearchPattern,
-                ["CONTENT"] = CurrentCache.Setting.FindFileContent
-                    ? CurrentCache.Setting.FileContentSearchPattern
+                ["COUNT"] = count.ToString(),
+                ["DIR"] = setting.RootDirectoryPath,
+                ["NAME"] = setting.FileNameSearchPattern,
+                ["CONTENT"] = setting.FindFileContent
+                    ? setting.FileContentSearchPattern
                     : Properties.Resources.String_Finder_FindGroup_Output_NotSelected
                 ,
             };
-            var titleText = CommonUtility.ReplaceNKitText(Properties.Resources.String_Finder_FindGroup_Output_Title_Format, CurrentCache.Setting.UpdatedUtcTimestamp, titleMap);
+            var titleText = CommonUtility.ReplaceNKitText(Properties.Resources.String_Finder_FindGroup_Output_Title_Format, setting.UpdatedUtcTimestamp, titleMap);
             writer.WriteLine(titleText);
+        }
+
+        static void WriteListFileFindItem(TextWriter writer, bool absolutePath, bool isSimple, FindItemModel findItemModel)
+        {
+            if(absolutePath) {
+                writer.WriteLine(findItemModel.FileInfo.FullName);
+            } else {
+                writer.WriteLine(findItemModel.RelativeDirectoryPath);
+            }
+        }
+
+        void WriteListFile(TextWriter writer, bool absolutePath, bool isSimple, IReadOnlyList<int> outputitemsIndex)
+        {
+            WriteListFileTitle(writer, CurrentCache.Setting, outputitemsIndex.Count);
+
             writer.WriteLine();
 
             foreach(var index in outputitemsIndex) {
-                var file = Items[index];
-                if(absolutePath) {
-                    writer.WriteLine(file.FileInfo.FullName);
-                } else {
-                    writer.WriteLine(file.RelativeDirectoryPath);
-                }
+                var findItemModel = Items[index];
+                WriteListFileFindItem(writer, absolutePath, isSimple, findItemModel);
             }
         }
 
         public void OutputListFile(string outputPath, bool absolutePath, bool isSimple, IReadOnlyList<int> outputitemsIndex)
         {
             using(var writer = System.IO.File.CreateText(outputPath)) {
-                OutputListFileCore(writer, absolutePath, isSimple, outputitemsIndex);
+                WriteListFile(writer, absolutePath, isSimple, outputitemsIndex);
             }
         }
 
